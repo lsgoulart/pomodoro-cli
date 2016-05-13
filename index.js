9b1fac775429be49e4425ba32d5ae7da5420c6de
Leonardo Goulart <leonardo.sgoulart@gmail.com>
2016-05-12 21:25:45 -0300

    Starting the counter

--- index.js
+++ index.js
@@ -3,30 +3,31 @@
 /**
 * Module dependencies.
 */
-
 var program = require('commander');
 var pkg = require('./package');
 var Config = require('configstore');
 var moment = require('moment');
 var notifier = require('node-notifier');
+var Spinner = require('cli-spinner').Spinner;
 
 /**
  * Initial config store
  */
-var config = new Config(pkg.name)
+var config = new Config(pkg.name);
+var spinner = new Spinner('Countdown %s');
 
 program
     .version('0.0.1')
     .option('-s, --start', 'Pomodoro Start')
-    .option('-p, --pause', "Pomodoro Pause")
-    .option('-b, --shortbreak', "Short Break")
-    .option('-B, --longbreak', "Long Break")
+    .option('-p, --pause', 'Pomodoro Pause')
+    .option('-b, --shortbreak', 'Short Break')
+    .option('-B, --longbreak', 'Long Break')
     .parse(process.argv);
 
 var getDuration = function() {
     var duration = config.get('time');
     if(duration > 0) {
-        duration -= 1000;
+        duration -= 100;
         config.set('time', duration);
     }
 
@@ -47,7 +48,10 @@ var startTimer = function(type) {
             time = (60*25)*1000;
             break;
     }
+
     config.set('time', time);
+    spinner.setSpinnerString(18);
+    spinner.start();
 
     var interval = setInterval(function() {
         var duration = getDuration();
@@ -59,8 +63,10 @@ var startTimer = function(type) {
             });
         }
 
-        process.stdout.clearLine();
-		process.stdout.write('\r' + moment.utc(duration).format('mm:ss'));
+        spinner.setSpinnerTitle(type + ': ' + moment.utc(duration).format('mm:ss'));
+
+        //process.stdout.clearLine();
+		//process.stdout.write('\rPomodoro: ' + moment.utc(duration).format('mm:ss'));
     }, 100);
 };
 
